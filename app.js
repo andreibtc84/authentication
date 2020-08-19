c4da2388d2794c97fb89ff0ca96eb9e27aa1b27c
root <andreibtc@gmail.com>
2020-08-19 16:03:59 +0000

    Added register form Added register routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/auth_app', {useNewUrlParser: true, u
 
 var app = express();
 app.set("view engine" , "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(require("express-session")({
 	secret: "This phrase is used to encode and decode the sessions",
@@ -24,6 +25,13 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
+//==============================
+//      ROUTES
+//==============================
+
+
+
 app.get("/", function(req, res){
 	res.render("home");
 });
@@ -31,6 +39,32 @@ app.get("/", function(req, res){
 app.get("/secret", function(req, res){
 	res.render("secret");
 });
+
+// AUTH ROUTES
+
+// Show sign up form
+
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+// handling user sign up
+
+app.post("/register", function(req, res){
+	req.body.username
+	req.body.password
+	User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register");
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/secret");
+		});
+	});
+});
+
+
 
 
 app.listen(3000, function() { 
